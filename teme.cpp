a8b6bfe28d3e4b227ee097f08c9b5d4ffc379a58
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-15 13:12:22 +0200

    Create teme.cpp - scor 90

---
+++ teme.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#define MAX_TEME 301
+using namespace std;
+int nrTeme;
+bool elementComun[MAX_TEME][MAX_TEME],
+     esteGrupataTema[MAX_TEME];
+int numarMinimDeReviste();
+int main()
+{
+    int nrPerechi;
+    ifstream fin("teme.in");
+    fin >> nrTeme >> nrPerechi;
+    while (nrPerechi > 0)
+    {
+        int i, j;
+        fin >> i >> j;
+        elementComun[i][j] = elementComun[i][j] = 1;
+        nrPerechi--;
+    }
+    fin.close();
+    ofstream fout("teme.out");
+    fout << numarMinimDeReviste();
+    fout.close();
+    return 0;
+}
+void grupeazaTema(int i)
+{
+    esteGrupataTema[i] = 1;
+    for (int j = 1; j <= nrTeme; j++)
+        if (elementComun[i][j] && !esteGrupataTema[j])
+            grupeazaTema(j);
+}
+int numarMinimDeReviste()
+{
+    int nrReviste = 0;
+    for (int i = 1; i <= nrTeme; i++)
+        if (!esteGrupataTema[i])
+        {
+            nrReviste++;
+            grupeazaTema(i);
+        }
+    return nrReviste;
+}
